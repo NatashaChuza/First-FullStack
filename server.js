659dcc0287b479640eb083f41145f10ba46e9ca1
Natasha <natashachuza@gmail.com>
2020-04-13 16:40:33 +0200

    created user model

--- server.js
+++ server.js
@@ -1,27 +1,26 @@
-const express = require('express')
-const mongoose = require('mongoose')
-const bodyParser = require('body-parser')
+const express = require("express");
+const mongoose = require("mongoose");
+const bodyParser = require("body-parser");
 
-const app = express()
+const app = express();
 
 //body parser middleware
 app.use(
-    bodyParser.urlencoded({
-        extended: false
-    })
-)
-app.use(bodyParser.json())
+  bodyParser.urlencoded({
+    extended: false,
+  })
+);
+app.use(bodyParser.json());
 
 //db config
-const db = require("./config/keys").mongoURI 
+const db = require("./config/keys").mongoURI;
 
 //connect to monogDB
-mongoose.connect(
-    db,
-    { useNewUrlParser: true}
-).then( ()=> console.log("MongoDB successfully connected"))
-  .catch( err => console.log(err))
+mongoose
+  .connect(db, { useNewUrlParser: true })
+  .then(() => console.log("MongoDB successfully connected"))
+  .catch((err) => console.log(err));
 
-  const port = process.env.PORT || 5000
+const port = process.env.PORT || 5000;
 
-  app.listen( port, ()=> console.log(`server up and running on port ${port}...`))
+app.listen(port, () => console.log(`server up and running on port ${port}...`));
